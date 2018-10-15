37addca3fa8137028ecd2c7a0453ab035e3c5b05
RobinFischer <robin.fischer7@web.de>
2018-10-16 00:46:10 +0200

    add base project

---
+++ Assets/MQTT/scripts/test/mqttTest.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using uPLibrary.Networking.M2Mqtt.Utility;
+using uPLibrary.Networking.M2Mqtt.Exceptions;
+
+using System;
+using System.Timers;
+using System.Text;
+using System.Collections.Generic;
+
+public class mqttTest : MonoBehaviour
+{
+    public string MQTT_val;
+
+    private MqttClient client;
+    private testJSON testMessage;
+    bool firstFlag;
+    // Use this for initialization
+    void Start()
+    {
+        firstFlag = false;
+        // create client instance 
+        client = new MqttClient(IPAddress.Parse("127.0.0.1"), 1883, false, null);
+
+        string usertempar = "default";
+        string passtempar = "default";
+
+        // register to message received 
+        client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+
+        string clientId = Guid.NewGuid().ToString();
+        client.Connect(clientId, usertempar, passtempar);
+
+        // subscribe to the topic "/home/temperature" with QoS 2 
+        client.Subscribe(new string[] { "hello/world" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+        //client.Subscribe(new string[] { "#" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+    }
+    void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+    {
+        //Debug.Log(e.Message.ToString());
+
+        if (!firstFlag)
+        {
+            testMessage = JsonUtility.FromJson<testJSON>(Encoding.UTF8.GetString(e.Message));
+            firstFlag = true;
+            Debug.Log("First Message Received!");
+        }
+        else
+            JsonUtility.FromJsonOverwrite(Encoding.UTF8.GetString(e.Message), testMessage);
+
+
+        Debug.Log(testMessage.testVal);
+        MQTT_val = testMessage.testVal;
+
+        Debug.Log("Received: " + Encoding.UTF8.GetString(e.Message));
+    }
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(20, 40, 80, 20), "Level 1"))
+        {
+            Debug.Log("sending...");
+            client.Publish("hello/world", System.Text.Encoding.UTF8.GetBytes("Sending from Unity3D!!!"), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
+            Debug.Log("sent");
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+    }
+
+    [Serializable]
+    public class testJSON
+    {
+        public string testVal;
+    }
+}
---
+++ Assets/Scripts/MQTT_Manager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MQTT_Manager : MonoBehaviour {
+
+    public mqttTest MQTT_in;
+    public TextMesh Mqtt_out;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Mqtt_out.text = "Received MQTT Value:" + MQTT_in.MQTT_val;
+	}
+}
